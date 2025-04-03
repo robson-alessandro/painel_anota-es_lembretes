73778d1b58a72b77681ac0f85e0eec90c1aa0b47
robson-alessandro <robsonalessandro74@gmail.com>
2025-04-03 18:22:53 -0300

    criando card

---
+++ src/componentes/Card/index.js
@@ -0,0 +1,16 @@
+import './Card.css';
+const Card =()=>{
+    return(
+        <div className='card'>
+            <div className='cabecalho-card'>
+                <h2>data</h2>
+                <h2>titulo</h2>
+            </div>
+            
+            <p>conteudo</p>
+        </div>
+
+    )
+}
+
+export default Card;
--- src/componentes/Mural/index.js
+++ src/componentes/Mural/index.js
@@ -1,8 +1,9 @@
+import Card from '../Card';
 import './Mural.css'
 const Mural = () =>{
     return(
         <div className="conteudo_mural">
-            conteudo...
+            <Card/>
            
         </div>
         
